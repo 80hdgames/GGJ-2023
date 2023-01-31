bdc67108b6599bb4f68ba6bc8cbe3c25d268ceee
Jonathan Niemi <jontniemi@gmail.com>
2023-01-31 12:25:14 -0500

    Added HUDTimer prefab & script
    -timer label updates to match Timer's time_left, prints message on timeout

---
+++ GGJ-2023-Project/Scripts/UI/HUDTimer.gd
@@ -0,0 +1,25 @@
+extends Label
+
+@onready var timer :Timer = $Timer
+var lastValue :int
+
+
+func _ready():
+	timer.timeout.connect(_on_timeout)
+	timer.start()
+	_update_label()
+	pass
+
+
+func _on_timeout():
+	print("GAME OVER!")
+	pass
+
+
+func _process(_delta):
+	_update_label()
+	
+	
+func _update_label():
+	lastValue = int(timer.time_left)
+	text = str(lastValue)
