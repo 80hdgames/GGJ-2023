cd3df97a50bb3b5afa2971c7848c10910cea1297
Chris Gengler <tayrtahn@gmail.com>
2023-01-31 12:18:39 -0500

    Squashed commit of the following:
    commit 250effcacfe580f85add53799716cf3bc53635a6
    Merge: a1204c8 aabee2f
    Author: Chris Gengler <tayrtahn@gmail.com>
    Date:   Tue Jan 31 12:17:56 2023 -0500
    
        Merge branch 'main' into Boost
    
    commit a1204c82421672b96ddc4145417aa1159850b9e3
    Author: Chris Gengler <tayrtahn@gmail.com>
    Date:   Tue Jan 31 12:16:54 2023 -0500
    
        Added ability to use temporary speed boosts

--- GGJ-2023-Project/Scripts/Player.gd
+++ GGJ-2023-Project/Scripts/Player.gd
@@ -4,17 +4,23 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const TITLE_SCREEN_SCENE = Constants.TITLE_SCREEN_SCENE
+const MAX_BOOST = 1
+const BOOST_SPEED = 4
+const BOOST_RECHARGE_RATE = 0.01
+const BOOST_BURN_RATE = 0.02
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-
+var boostCharge = MAX_BOOST
+var isBoosting = false
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		SceneManager.go_to(TITLE_SCREEN_SCENE)
 
-
+func can_boost():
+	return boostCharge >= 1 and !isBoosting
+	
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,14 +30,26 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("desktop1_boost") and can_boost():
+		isBoosting = true
+		
+	if isBoosting:
+		boostCharge = max(0, boostCharge - BOOST_BURN_RATE)
+		if boostCharge <= 0:
+			isBoosting = false
+	else:
+		boostCharge = min(boostCharge + BOOST_RECHARGE_RATE, MAX_BOOST)
+
+	var boost = BOOST_SPEED if isBoosting else 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * (SPEED + boost)
+		velocity.z = direction.z * (SPEED + boost)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
