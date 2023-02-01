253c2e49ba08c97a65555ea76791e6b2d55063c9
Jonathan Niemi <jontniemi@gmail.com>
2023-02-01 12:45:29 -0500

    Added exit from player setup scene
    -'ui_cancel' goes back to title

--- GGJ-2023-Project/Scripts/PlayerSetup.gd
+++ GGJ-2023-Project/Scripts/PlayerSetup.gd
@@ -1,8 +1,11 @@
 extends Node
 
 const GAME_SCENE = Constants.GAME_SCENE
+const TITLE_SCREEN_SCENE = Constants.TITLE_SCREEN_SCENE
 
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):
 		SceneManager.go_to(GAME_SCENE)
+	elif event.is_action_pressed("ui_cancel"):
+		SceneManager.go_to(TITLE_SCREEN_SCENE)
