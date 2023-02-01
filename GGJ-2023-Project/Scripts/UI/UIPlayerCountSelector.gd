348e9446c69f8b6ec70993fcf7dae0c913ec9b7d
Jonathan Niemi <jontniemi@gmail.com>
2023-02-01 12:46:10 -0500

    Player setup improvements
    -added menu navigate sounds when changing player count
    -added resetting player count to 1 on start

--- GGJ-2023-Project/Scripts/UI/UIPlayerCountSelector.gd
+++ GGJ-2023-Project/Scripts/UI/UIPlayerCountSelector.gd
@@ -1,14 +1,18 @@
 extends HSlider
 
+const SoundType = SfxManager.SoundType
+
 @onready var label :Label = $Label
 
 
 func _ready():
+	_update_players(1)
 	grab_focus()
 	value_changed.connect(_update_players)
 
 
 func _update_players(new_value):
+	SfxManager.enqueue2d(SoundType.MenuNavigate)
 	PlayerManager.set_player_count(int(new_value))
 	label.text = ("%s Player" % new_value)
 
