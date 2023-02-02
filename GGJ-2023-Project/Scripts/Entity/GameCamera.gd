f29017db58e82d1ba4b86ac435f608a1185f760a
Chris Gengler <tayrtahn@gmail.com>
2023-02-02 12:21:03 -0500

    Increased speed and maximum zoom of camera

--- GGJ-2023-Project/Scripts/Entity/GameCamera.gd
+++ GGJ-2023-Project/Scripts/Entity/GameCamera.gd
@@ -2,6 +2,8 @@ extends Camera3D
 
 const PLAYER_NODE_GROUP = Constants.PLAYER_NODE_GROUP
 const CAM_SPEED = 2.0
+const CAM_ZOOM_SPEED = 10.0
+const CAM_ZOOM_MAX = 20.0
 const EXTENTS = 5.0
 
 var _players :Array = []
@@ -32,5 +34,5 @@ func _physics_process(_delta):
 #	look_at(averagePos)
 	global_position.x = lerp(global_position.x, averagePos.x, CAM_SPEED * _delta)
 #	global_position.x = clamp(global_position.x, -EXTENTS, EXTENTS)
-	_smoothedFovAdjust = move_toward(fov, _defaultFov + clamp(_maxSqrDistance, 0.0, 10.0), _delta)
+	_smoothedFovAdjust = move_toward(fov, _defaultFov + clamp(_maxSqrDistance, 0.0, CAM_ZOOM_MAX), CAM_ZOOM_SPEED * _delta)
 	fov = _smoothedFovAdjust
