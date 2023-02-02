be98187a285473d1b49fa2d4f766c8f7ad62af25
Jonathan Niemi <jontniemi@gmail.com>
2023-02-02 12:10:39 -0500

    Added a simple fov adjust for game camera
    -camera seemingly zooms out via fov adjust based on pig x position

--- GGJ-2023-Project/Scripts/Entity/GameCamera.gd
+++ GGJ-2023-Project/Scripts/Entity/GameCamera.gd
@@ -5,6 +5,9 @@ const CAM_SPEED = 2.0
 const EXTENTS = 5.0
 
 var _players :Array = []
+var _maxSqrDistance :float = 0.0
+@onready var _smoothedFovAdjust :float = fov
+@onready var _defaultFov :float = fov
 
 
 func _ready():
@@ -16,14 +19,18 @@ func _acquire_targets():
 
 
 func _physics_process(_delta):
+	_maxSqrDistance = 0.0
 	if _players.is_empty():
 		return
 		
 	var averagePos :Vector3 = Vector3.ZERO
 	for p in _players:
+		_maxSqrDistance = max(_maxSqrDistance, abs(global_transform.origin.x - p.global_transform.origin.x))
 		averagePos += p.global_transform.origin
 	averagePos /= _players.size()
 	
 #	look_at(averagePos)
 	global_position.x = lerp(global_position.x, averagePos.x, CAM_SPEED * _delta)
 #	global_position.x = clamp(global_position.x, -EXTENTS, EXTENTS)
+	_smoothedFovAdjust = move_toward(fov, _defaultFov + clamp(_maxSqrDistance, 0.0, 10.0), _delta)
+	fov = _smoothedFovAdjust
