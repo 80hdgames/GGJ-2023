f45e7378f1d51963ce2266c95b436dbfc77a570e
Chris Gengler <tayrtahn@gmail.com>
2023-02-02 14:14:00 -0500

    Camera now follows pigs on Z axis too
    * Camera position is weighted towards the center of the playfield

--- GGJ-2023-Project/Scripts/Entity/GameCamera.gd
+++ GGJ-2023-Project/Scripts/Entity/GameCamera.gd
@@ -4,6 +4,8 @@ const PLAYER_NODE_GROUP = Constants.PLAYER_NODE_GROUP
 const CAM_SPEED = 2.0
 const CAM_ZOOM_SPEED = 10.0
 const CAM_ZOOM_MAX = 20.0
+const CAM_OFFSET_Z = 8
+const CAM_CENTER_POWER = 3
 const EXTENTS = 5.0
 
 var _players :Array = []
@@ -29,10 +31,14 @@ func _physics_process(_delta):
 	for p in _players:
 		_maxSqrDistance = max(_maxSqrDistance, abs(global_transform.origin.x - p.global_transform.origin.x))
 		averagePos += p.global_transform.origin
+		
 	averagePos /= _players.size()
+	
+	averagePos /= CAM_CENTER_POWER
 	
 #	look_at(averagePos)
 	global_position.x = lerp(global_position.x, averagePos.x, CAM_SPEED * _delta)
+	global_position.z = lerp(global_position.z, averagePos.z + CAM_OFFSET_Z, CAM_SPEED * _delta)
 #	global_position.x = clamp(global_position.x, -EXTENTS, EXTENTS)
 	_smoothedFovAdjust = move_toward(fov, _defaultFov + clamp(_maxSqrDistance, 0.0, CAM_ZOOM_MAX), CAM_ZOOM_SPEED * _delta)
 	fov = _smoothedFovAdjust
